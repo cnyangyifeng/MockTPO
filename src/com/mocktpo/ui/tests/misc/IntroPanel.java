3748384b158ff07e4e0f65cae0a5e376de97f957
YANG YIFENG <165239796@qq.com>
2015-11-23 23:13:03 +0800

    增加了考试介绍部分

---
+++ src/com/mocktpo/ui/tests/misc/IntroPanel.java
@@ -0,0 +1,62 @@
+package com.mocktpo.ui.tests.misc;
+
+import com.mocktpo.ui.widgets.BodyPanel;
+import com.mocktpo.util.GlobalConstants;
+
+import javax.swing.*;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.StyleSheet;
+import java.awt.*;
+
+public class IntroPanel extends BodyPanel {
+
+    public static final int DESCRIPTION_PANE_WIDTH = 800;
+    public static final int DESCRIPTION_PANE_HEIGHT = 400;
+
+    private JEditorPane descriptionPane;
+
+    public IntroPanel(Rectangle bounds) {
+        super(bounds);
+        this.initComponents();
+    }
+
+    private void initComponents() {
+        this.setLayout(null);
+        this.setDescriptionPane();
+    }
+
+    private void setDescriptionPane() {
+        this.descriptionPane = new JEditorPane();
+
+        int x = (this.getWidth() - DESCRIPTION_PANE_WIDTH) / 2;
+        int y = (this.getHeight() - DESCRIPTION_PANE_HEIGHT) / 2;
+        this.descriptionPane.setBounds(x, y, DESCRIPTION_PANE_WIDTH, DESCRIPTION_PANE_HEIGHT);
+
+        this.descriptionPane.setEditable(false);
+        this.descriptionPane.setOpaque(false);
+
+        HTMLEditorKit kit = new HTMLEditorKit();
+        StyleSheet style = kit.getStyleSheet();
+        style.addRule(".intro-img { margin-bottom: 40px; text-align: center; } .intro-img img { height: 90px; width: 160px; } .intro-text { color: #333333; font-family: Arial; font-size: 12px; text-align: center; }");
+        this.descriptionPane.setEditorKit(kit);
+        String imgUrl = this.getClass().getResource(GlobalConstants.IMAGES_DIR + "ets_toefl.png").toString();
+        String text = "<div class='intro-img'><img src='" + imgUrl + "' /></div>";
+        text += "<div class='intro-text'>Copyright 2009, 2011 by Educational Testing Service. All rights reserved.<br />EDUCATIONAL TESTING SERVICE, ETS, the ETS logo, TOEFL and TOEFL iBT are registered trademarks of<br />Educational Testing Service (ETS) in the United States and other countries.<br /><br />Click on <b>Continue</b> to go on.</div>";
+        this.descriptionPane.setText(text);
+
+        this.add(this.descriptionPane);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        int width = this.getWidth();
+        int height = this.getHeight();
+
+        Graphics2D g2d = (Graphics2D) g;
+        Color bg = new Color(238, 213, 204);
+        g2d.setPaint(bg);
+        g2d.fillRect(0, 0, width, height);
+    }
+}
---
+++ src/com/mocktpo/ui/windows/IntroFrame.java
@@ -0,0 +1,107 @@
+package com.mocktpo.ui.windows;
+
+import com.mocktpo.ui.dialogs.PauseDialog;
+import com.mocktpo.ui.tests.misc.IntroPanel;
+import com.mocktpo.ui.widgets.MButton;
+import com.mocktpo.util.GlobalConstants;
+import com.mocktpo.util.LayoutConstants;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class IntroFrame extends TestFrame implements ActionListener {
+
+    // Constants
+
+    public static final int CONTINUE_BUTTON_WIDTH = 74;
+    public static final int CONTINUE_BUTTON_HEIGHT = 34;
+
+    // Components
+
+    private MButton continueButton;
+    private IntroPanel introPanel;
+
+    public IntroFrame(GraphicsConfiguration gc, MainFrame mainFrame) {
+        super(gc, mainFrame);
+    }
+
+    protected void configData() {
+    }
+
+    protected void customizeHeaderPanel() {
+        this.setControlButtons();
+    }
+
+    protected void customizeBodyPanel() {
+        this.introPanel = new IntroPanel(this.bodyBounds);
+        this.bodyPanel = this.introPanel;
+    }
+
+    /**************************************************
+     * Private methods
+     **************************************************/
+
+    private void setControlButtons() {
+        this.setContinueButton();
+    }
+
+    private void setContinueButton() {
+        this.continueButton = new MButton();
+
+        int x = this.headerPanel.getWidth() - CONTINUE_BUTTON_WIDTH - LayoutConstants.MARGIN * 2;
+        int y = LayoutConstants.MARGIN * 3;
+        this.continueButton.setBounds(x, y, CONTINUE_BUTTON_WIDTH, CONTINUE_BUTTON_HEIGHT);
+
+        ImageIcon icon = new ImageIcon(this.getClass().getResource(GlobalConstants.IMAGES_DIR + "continue.png"));
+        this.continueButton.setIcon(icon);
+        ImageIcon rolloverIcon = new ImageIcon(this.getClass().getResource(GlobalConstants.IMAGES_DIR + "continue_hi.png"));
+        this.continueButton.setRolloverIcon(rolloverIcon);
+        this.continueButton.setText(null);
+        this.continueButton.setMargin(new Insets(0, 0, 0, 0));
+        this.continueButton.setBorder(null);
+        this.continueButton.setBorderPainted(false);
+        this.continueButton.setFocusPainted(false);
+        this.continueButton.setContentAreaFilled(false);
+
+        this.continueButton.setActionCommand("doContinue");
+        this.continueButton.addActionListener(this);
+
+        this.headerPanel.add(this.continueButton);
+    }
+
+    /**************************************************
+     * Listeners
+     **************************************************/
+
+    public void actionPerformed(ActionEvent e) {
+        String ac = e.getActionCommand();
+        switch (ac) {
+            case "doPauseTest":
+                logger.info("'Pause Test' button pressed.");
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        PauseDialog pause = new PauseDialog(IntroFrame.this, "", true);
+                        pause.setVisible(true);
+                    }
+                });
+                break;
+            case "doContinue":
+                logger.info("'Continue' button pressed.");
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        getContentPane().remove(bodyPanel);
+                        //
+                        getContentPane().add(bodyPanel);
+                        repaint();
+                    }
+                });
+                break;
+            default:
+                break;
+        }
+    }
+}

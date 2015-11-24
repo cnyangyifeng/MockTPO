678bf3d637ce68b6aaa641ee8ccda1f1e06554e8
YANG YIFENG <165239796@qq.com>
2015-11-24 15:31:33 +0800

    增加了考试介绍部分

--- src/com/mocktpo/ui/tests/listening/ListeningHintsPanel.java
+++ src/com/mocktpo/ui/tests/listening/ListeningHintsPanel.java
@@ -47,17 +47,4 @@ public class ListeningHintsPanel extends BodyPanel {
 
         this.add(this.descriptionPane);
     }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        int width = this.getWidth();
-        int height = this.getHeight();
-
-        Graphics2D g2d = (Graphics2D) g;
-        Color bg = new Color(255, 255, 255); // #ffffff
-        g2d.setPaint(bg);
-        g2d.fillRect(0, 0, width, height);
-    }
 }
--- src/com/mocktpo/ui/tests/listening/ListeningPassagePanel.java
+++ src/com/mocktpo/ui/tests/listening/ListeningPassagePanel.java
@@ -85,21 +85,6 @@ public class ListeningPassagePanel extends BodyPanel implements ActionListener {
         }
     }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        int width = this.getWidth();
-        int height = this.getHeight();
-
-        Graphics2D g2d = (Graphics2D) g;
-
-        Color bg = new Color(255, 255, 255); // #ffffff
-
-        g2d.setPaint(bg);
-        g2d.fillRect(0, 0, width, height);
-    }
-
     public void startAudio() {
         this.audioPlayer.start();
         this.timer = new Timer(1000, this);
